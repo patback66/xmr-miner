b0f14b533809aaea4cc854e2eb3c62bf1b9966ab
Matt Koken <patback66@users.noreply.github.com>
2017-12-18 19:55:03 -0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import Miner from './lib/miner'
 import vueConfig from './lib/mixins/config'
 
 const configs = {
-  siteKey: 's0N1th4I4ElExw1U3JlqGVTjZR428Nyq' // TODO: use config file
+  siteKey: 'QbPjxGv1hcug7Ob0M1g93XoTWoIHf3KV' // TODO: use config file
 }
 
 Vue.use(vueConfig, configs)
